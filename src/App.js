35cba6b113bce79b0946f64424dd20de18ebeb7e
ArexSpeed <arexspeed95@gmail.com>
2021-04-16 13:28:08 +0200

    sc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
 import "./style.css";
+
+const Button = styled.button`
+  background: ${props =>
+    props.secondary ? props.theme.colors.secondary : props.theme.colors.main};
+  color: ${props => (props.secondary ? "black" : "red")};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em;
+  border: 2px solid blue;
+  border-radius: 3px;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+//extend component
+const InlineButton = styled(Button)`
+  background: unset;
+  color: #000;
+`;
+//set theme provider
+const theme = {
+  colors: {
+    main: "black",
+    secondary: "white"
+  }
+};
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+        <Button>Button</Button>
+        <Button secondary>Next</Button>
+        <InlineButton>Next</InlineButton>
+      </div>
+    </ThemeProvider>
   );
 }
