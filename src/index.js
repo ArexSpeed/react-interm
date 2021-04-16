5ee5d2eabb7678329a75045b5a8dc52ffffeffce
ArexSpeed <arexspeed95@gmail.com>
2021-04-16 14:53:05 +0200

    middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import App from "./App";
 
-const store = createStore(rootReducer);
+//middleware (store(dispatch, getState) => next(action to bottom) => action)
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info("dispatching", action);
+  //next zapobiega zapetleniu sie akcji dispatch
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+
+  return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
